26758a5d3b52dbb28187fe120ab4716afd58ebbf
Aaron DuChateau <chateauconcept@gmail.com>
2020-12-06 17:55:36 -0800

    test vue 3 reactivity

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Resources from '../views/Resources.vue'
+import TestArea from '../views/TestArea.vue'
 
 const routes = [
   {
@@ -21,6 +22,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: About
+  },
+  {
+    path: '/test-area',
+    name: 'TestArea',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: TestArea
   }
 ]
 
