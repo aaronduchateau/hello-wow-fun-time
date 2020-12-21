23619b9b62823ad851cdbc6b6f425528a2dfd060
Aaron DuChateau <chateauconcept@gmail.com>
2020-12-21 14:52:08 -0800

    add google analytics

--- src/views/StepOne.vue
+++ src/views/StepOne.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="full-page-holder">
     <div class="instructions">
-      <h1>Pool Entry</h1>
+      <h1>Pool Entry</h1> 
       <div class="maxWidth500Center">
         <h2>This is step 1.</h2>
         The goal of this step is aquire Ethereum (ticker symbol: <b>ETH</b>) using a
@@ -25,9 +25,9 @@
         In order to do this you are going to need to use an <b>Exchange</b> to turn money
         from your bank account into ETH. In order to send your ETH to the pool, you will
         likely need to use: <br /><br />
-        <a href="https://kraken.com" target="_blank">Kraken</a>,
-        <a href="https://coinbase.com" target="_blank">Coinbase</a>, or
-        <a href="https://bittrex.com" target="_blank">Bittrex</a>. <br /><br />
+        <a href="https://r.kraken.com/c/2601920/696580/10583" target="_blank">Kraken</a>,
+        Coinbase, or
+        Bittrex. <br /><br />
 
         You might be tempted to google another exchange to buy your <b>ETH</b>, but
         <i>BEWARE</i>, services like Paypal, CashApp, and Robinhood do NOT allow you to
@@ -40,7 +40,7 @@
         <b>Note:</b> If you are linking a bank account, recieving your ETH may take 3-5
         days. That's because banks are slow. While many of these exchanges have different
         characteristics, we <i>currently</i> believe that the
-        <a href="https://kraken.com" target="_blank">Kraken</a> exchange will get you an available balance the fastest. <br /><br />
+        <a href="https://r.kraken.com/c/2601920/696580/10583" target="_blank">Kraken</a> exchange will get you an available balance the fastest. <br /><br />
         Regardless of the exchange you choose (from the approved 3 above) the process will
         be mostly the same. You will need to verify your identiy with photo id (just like
         you are getting a new bank account), hook up some form of payment system, and
